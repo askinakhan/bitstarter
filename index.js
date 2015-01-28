b18166d8bc7484a8e5b181915ac91e9c9c58dc5a
Askin Akhan <askinakhan@gmail.com>
2015-01-28 19:11:06 +0000

    Change

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 var express = require('express')
 var app = express()
-var buff = fs.readFileSync('index.html'); 
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('buff');
+  response.send('Hello World');
 })
 
 app.listen(app.get('port'), function() {
